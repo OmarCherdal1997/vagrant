485d7d9c45750d653fea3f217608de7d98c2be31
Omar Cherdal <45284581+OmarCherdal1997@users.noreply.github.com>
2022-02-17 12:15:53 +0100

    Install Nginx Ingress

--- k8s/install_master.sh
+++ k8s/install_master.sh
@@ -170,3 +170,11 @@ alias k='kubectl'
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+ ## Install Nginx Ingress
+ helm repo add nginx-stable https://helm.nginx.com/stable
+ helm repo update
+ helm install nginx-ingress nginx-stable/nginx-ingress
+ 
+ 
+ 
